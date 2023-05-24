3797d217746b0fdf8bd31ef20a73f269dfdeab99
Andres Bonilla <andy.cbr.tr@gmail.com>
2023-05-24 12:39:07 -0500

    Entregable 4 completo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const userRouter = require('./user.routes');
 const router = express.Router();
 
-// colocar las rutas aquí
+// colocar las rutas 
+//rutas publicas
+router.use('/users',userRouter)
+
+//rutas protegidas
 
 
 module.exports = router;
